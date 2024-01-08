62501c3e211ce8272c652b721560a606e837f83b
César Alexis Abarca Reyes <cabarcar@outlook.com>
2024-01-07 19:38:03 -0600

    Server js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/aba-game'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/aba-game/index.html'));});
+
+// Aquí ajustamos la ruta para apuntar al subdirectorio 'browser'
+const directoryPath = path.join(__dirname, 'dist', 'aba-game', 'browser');
+app.use(express.static(directoryPath));
+
+app.get('/*', function(req, res) {
+    // Asegúrate de que el path para enviar el archivo sea el correcto
+    res.sendFile(path.join(directoryPath, 'index.html'));
+});
+
+// El puerto sigue siendo el mismo, ya sea el asignado por Heroku o 8080 por defecto
 app.listen(process.env.PORT || 8080);
+
